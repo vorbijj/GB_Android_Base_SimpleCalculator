63620c17383d5896b974ac5292badf278f56dcde
Ilya Vorobyev <vorbinn@list.ru>
2021-06-06 15:22:04 +0300

    Создал проект Simple Calculator

---
+++ app/src/main/java/com/example/gb_android_base_simplecalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.gb_android_base_simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
