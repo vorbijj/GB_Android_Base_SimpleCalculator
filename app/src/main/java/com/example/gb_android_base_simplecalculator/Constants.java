fb2f6c16595085037d15c30b5a4f12e3d15b779d
Ilya Vorobyev <vorbinn@list.ru>
2021-07-09 01:44:38 +0300

    Реализация второй активити с настройками. Выбор и установка темы.

---
+++ app/src/main/java/com/example/gb_android_base_simplecalculator/Constants.java
@@ -0,0 +1,8 @@
+package com.example.gb_android_base_simplecalculator;
+
+public interface Constants {
+    String APP_THEME = "APP_THEME";
+    String YOUR_SETTING = "YOUR_SETTING";
+    int LIGHT = 0;
+    int DARK = 1;
+}
